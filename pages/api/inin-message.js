c6dfde65d22d4515832cd2c0223a9cb4c01629c3
Miavy4 <miavy4@gmail.com>
2025-02-13 12:30:18 +0500

    Delete pages/api directory

--- pages/api/inin-message.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-    if (req.method === 'GET') {
-        const initialMessage = "Привет! Я Настя, как я могу помочь?";
-        res.status(200).json({ message: initialMessage });
-    } else {
-        res.setHeader('Allow', ['GET']);
-        res.status(405).end(`Method Not Allowed`);
-    }
-}
